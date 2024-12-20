6b1acb8284adce729a22bddb62e462e54597a932
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-12-19 22:22:24 -0500

    Continue open app functionality

--- src/intents.py
+++ src/intents.py
@@ -9,14 +9,16 @@ def get_intent(user_text):
 
     greetings = ["hello", "hi", "hey", "good morning", "good afternoon", "good evening"]
     time_queries = ["time", "clock", "what time", "current time", "tell me the time"]
-    browser_queries = ["browser"]
+    application_queries = ["browser", "file explorer"]
 
     for token in doc:
         if any(greet in user_text for greet in greetings):
             return "greeting"
         elif any(time_query in user_text for time_query in time_queries):
             return "time_query"
-        elif any(browser_query in user_text for browser_query in browser_queries):
-            return "browser"
+        elif any(application_query in user_text for application_query in application_queries):
+            for application_query in application_queries:
+                if application_query in user_text:
+                    return application_query
 
     return "unknown"
--- src/responder.py
+++ src/responder.py
@@ -1,12 +1,15 @@
 import pyttsx3
 import random
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 import webbrowser
 from intents import get_intent
 from utils import get_time
 
 
 def greet():
-    greetings = ["Hello, how are you?", "Hi, what are you doing?", "Hello, what do you need help with?", "Hello, what can I do for you?"]
+    greetings = ["Hello, how are you?", "Hi, what are you doing?", "Hello, what do you need help with?",
+                 "Hello, what can I do for you?"]
     return random.choice(greetings)
 
 
@@ -17,12 +20,21 @@ def tell_time():
 def open_default_browser():
     url = "https://google.com"
     webbrowser.open(url)
+    return "Your default browser has been opened."
+
+
+def open_file_explorer():
+    tk.Tk().withdraw()
+
+    fn = askopenfilename()
+    return "user chose" + fn
 
 
 COMMANDS = {
     "greeting": greet,
     "time_query": tell_time,
     "browser": open_default_browser,
+    "file_explorer": open_file_explorer,
 }
 
 
