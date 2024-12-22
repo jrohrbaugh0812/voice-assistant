647af1954b044a15c6fcff2182f2732885f0991c
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-12-21 22:07:09 -0500

    Listener now adjusts for ambient noise. Also, better fallback responses.

--- src/recognizer.py
+++ src/recognizer.py
@@ -1,10 +1,21 @@
 import speech_recognition as sr
+import random
+
+
+def fallback_response():
+    return random.choice([
+        "Sorry, I didn't understand that.",
+        "Could you rephrase?",
+        "I'm not sure I can help with that."
+    ])
 
 
 def recognize_speech():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
+        print("Adjusting for ambient noise...")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
         print("Listening...")
         audio = recognizer.listen(source)
 
@@ -14,7 +25,7 @@ def recognize_speech():
         return text
 
     except sr.UnknownValueError:
-        print("Sorry, I didn't catch that.")
+        print(fallback_response())
     except sr.RequestError:
         print("Sorry, there was an error with the request.")
     return None
